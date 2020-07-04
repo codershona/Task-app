3025b7a2ade32a945a65abdf837813ad6db61b7b
codershona <falgunimst95@gmail.com>
2020-07-04 12:32:24 +0200

    Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -23,11 +23,107 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-    	name: 'JOHN',
-    	age: 24
+    // db.collection('users').insertOne({
+    // 	name: 'JOHN',
+    // 	age: 24
 
-    })
+    // }, (error, result) => {
+    // 	if (error) {
+    // 		return console.log('Unable to insert user!!!')
+    // 	}
+
+    // 	console.log(result.ops)
+
+
+
+
+    // })
+
+    // db.collection('users').insertMany([
+    // {
+    // 	name: 'Johnny',
+    // 	age: 24
+
+
+    //    }, {
+          
+    //       name: 'Jenny',
+    //       age: 23
+
+    //    }
+
+    // ], (error, result) => {
+
+    // 	if (error) {
+    // 		return console.log('Unabled to insert Documents!!!')
+    // 	}
+
+    // 	console.log(result.ops)
+
+
+
+
+    // })
+
+
+    // db.collection('tasks').insertMany([
+
+    //  {
+    // 	description: 'Clean the house',
+    // 	completed: true
+
+    // },{
+    // 	description: 'Renew the inspection!!!',
+    // 	completed: false
+
+    // },{
+    // 	description: 'Pot Plants!!!',
+    // 	completed: false
+
+    // }
+
+
+    // 	], (error, result) => {
+    // 		if (error) {
+
+    // 			return console.log('Unabled to inserts tasks!!!')
+
+    // 		}
+
+    // 		console.log(result.ops)
+
+
+    // 	})
+
+
+   db.collection('tasks-testing').insertMany([
+
+     {
+    	description: 'Hello Node.js!!!',
+    	completed: true
+
+    },{
+    	description: 'Working on task-manager-app!!!',
+    	completed: false
+
+    },{
+    	description: 'Working with mongodb databases!!!',
+    	completed: false
+
+    }
+
+
+    	], (error, result) => {
+    		if (error) {
+
+    			return console.log('Unabled to inserts tasks 4!!!')
+
+    		}
+
+    		console.log(result.ops)
+
+
+    	})
 
 
 
