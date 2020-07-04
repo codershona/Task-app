04a6cdfb7d2c1c9c660b909cb5a50a993fe57cd6
codershona <falgunimst95@gmail.com>
2020-07-04 12:01:06 +0200

    Inserting and Connecting Documents

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,36 @@
 // CRUD create read update delete 
 
 const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+
+ const connectionURL = 'mongodb://127.0.0.1:27017'
+
+// const connectionURL = 'mongodb://localhost:27017'
+
+const databaseName = 'task-manager'
+
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    
+    if (error) {
+
+    	return console.log('Unable to connect to database!!!')
+
+    }
+
+    // console.log('Connected correctly!!!')
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+    	name: 'JOHN',
+    	age: 24
+
+    })
+
+
+
+
+})
+
