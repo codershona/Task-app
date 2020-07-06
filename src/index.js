c86ccfb3a1c9a142a1181917b5272bbb79c145c9
codershona <falgunimst95@gmail.com>
2020-07-06 17:27:09 +0200

    Integrating Async/Await

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 
     // console.log(req.body)
 
@@ -22,35 +22,65 @@ app.post('/users', (req, res) => {
 
 	const user = new User(req.body)
 
-	user.save().then(() => {
+	try {
+		await user.save()
 		res.status(201).send(user)
 
-	}).catch((e) => {
+	} catch (e) {
 		res.status(400).send(e)
-		// res.send(e)
+
+	}
+
+	// await user.save()
+
+	// user.save().then(() => {
+	// 	res.status(201).send(user)
+
+	// }).catch((e) => {
+	// 	res.status(400).send(e)
+	// 	// res.send(e)
 
 
-	})
+	// })
 
 })
 
 // second get handlers:
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
 	const _id = req.params.id
+
+		try {
+		const user = await User.findById(_id)
+		// res.send(users)
+        
+        if (!user) {
+        	return res.status(404).send()
+        }
+
+        res.send(user)
+
+
+	} catch (e) {
+      res.status(500).send()
+
+	}
+
+
+
      
-     User.findById(_id).then((user) => {
-       if (!user) {
-       	return res.status(404).send()
+     // User.findById(_id).then((user) => {
+     //   if (!user) {
+     //   	return res.status(404).send()
 
-       }
+     //   }
 
-       res.send(user)
+     //   res.send(user)
 
-     }).catch((e) => {
-     	res.status(500).send()
+     // }).catch((e) => {
+     // 	res.status(500).send()
 
-     })
+     // })
 
 
 	// console.log(req.params)
@@ -63,15 +93,25 @@ app.get('/users/:id', (req, res) => {
 
 // Resource Reading Endpoints: Part 1:
 
-app.get('/users', (req, res) => {
-	User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+	try {
+		const users = await User.find({})
 		res.send(users)
 
-	}).catch((e) => {
-
+	} catch (e) {
       res.status(500).send()
 
-	})
+	}
+
+	// User.find({}).then((users) => {
+	// 	res.send(users)
+
+	// }).catch((e) => {
+
+ //      res.status(500).send()
+
+	// })
 
 
 })
@@ -79,22 +119,41 @@ app.get('/users', (req, res) => {
 
 // task 2 :
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
 	const task = new Task(req.body)
 
-	task.save().then(() => {
+	try {
+		await task.save()
 		res.status(201).send(task)
 
-	}).catch((e) => {
+	} catch (e) {
 		res.status(400).send(e)
-	})
+
+	}
+
+	// task.save().then(() => {
+	// 	res.status(201).send(task)
+
+	// }).catch((e) => {
+	// 	res.status(400).send(e)
+	// })
 
 })
 
 
 // Challenge 3:
 
-app.get('/tasks', (req, res) => {
+app.get('/tasks', async (req, res) => {
+
+	try {
+		const tasks = await Task.find({})
+		res.send(tasks)
+
+	} catch (e) {
+      res.status(500).send()
+
+	}
+
 	Task.find({}).then((tasks) => {
 		res.send(tasks)
 	}).catch((e) => {
@@ -107,19 +166,35 @@ app.get('/tasks', (req, res) => {
 
 // second route:
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
 	const _id = req.params.id
 
-	Task.findById(_id).then((task) => {
+	try {
+		const task = await Task.findById(_id)
+
 		if (!task) {
 			return res.status(404).send()
 		}
 
 		res.send(task)
 
-	}).catch((e) => {
-		res.status(500).send()
-	})
+	} catch (e) {
+    
+    res.status(500).send()
+
+	}
+	
+
+	// Task.findById(_id).then((task) => {
+	// 	if (!task) {
+	// 		return res.status(404).send()
+	// 	}
+
+	// 	res.send(task)
+
+	// }).catch((e) => {
+	// 	res.status(500).send()
+	// })
 })
 
 
