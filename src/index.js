00b84636893c110c0574b41def1a5e3895ee6be4
codershona <falgunimst95@gmail.com>
2020-07-08 11:36:51 +0200

    Resource deleting Endpoints

--- src/index.js
+++ src/index.js
@@ -156,6 +156,59 @@ app.patch('/users/:id', async (req, res) => {
 })
 
 
+
+// Resource Deleting Endpoints:
+
+app.delete('/users/:id', async (req, res) => {
+
+
+	try {
+
+		const user = await User.findByIdAndDelete(req.params.id)
+
+		if (!user) {
+
+			return res.status(404).send()
+		}
+
+		res.send(user)
+
+	} catch (e) {
+
+      res.status(500).send(e)
+
+	}
+
+})
+
+// deleting tasks:
+
+app.delete('/tasks/:id', async (req, res) => {
+
+
+	try {
+
+		const task = await Task.findByIdAndDelete(req.params.id)
+
+		if (!task) {
+
+			return res.status(404).send()
+		}
+
+		res.send(task)
+
+	} catch (e) {
+
+      res.status(500).send(e)
+
+	}
+
+})
+
+
+
+
+
 // task 2 :
 
 app.post('/tasks', async (req, res) => {
