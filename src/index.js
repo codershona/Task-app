a544f9a6a49cc0e65c524cab7b8a1adc39c30967
codershona <falgunimst95@gmail.com>
2020-07-10 13:17:41 +0200

    The User/Task Relationship done

--- src/index.js
+++ src/index.js
@@ -26,6 +26,33 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+
+ const Task = require('./models/task')
+ const User = require('./models/user')
+
+ // The user/ task relationship"
+
+ const main = async () => {
+
+ 	// const task = await Task.findById('5f084af866dcfb1981069bc9')
+
+ 	// await task.populate('owner').execPopulate()
+
+ 	// console.log(task.owner)
+
+ 	// console.log(task)
+
+    const user = await User.findById('5f084af866dcfb1981069bc9')
+
+    await user.populate('tasks').execPopulate()
+
+    console.log(user.tasks)
+
+
+ }
+
+ main()
+
 // hiding private data:
 
 const pet = {
@@ -42,6 +69,49 @@ pet.toJSON = function () {
 
 
 console.log(JSON.stringify(pet))
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+//     console.log(token)
+
+//     const data = jwt.verify(token, 'thisismynewcourse')
+//     console.log(data)
+// }
+
+// myFunction()
+
+
+
+
+// const express = require('express')
+// require('./db/mongoose')
+// const userRouter = require('./routers/user')
+// const taskRouter = require('./routers/task')
+
+// const app = express()
+// const port = process.env.PORT || 3000
+
+// // app.use((req, res, next) => {
+// //     if (req.method === 'GET') {
+// //         res.send('GET requests are disabled')
+// //     } else {
+// //         next()
+// //     }
+// // })
+
+// // app.use((req, res, next) => {
+// //     res.status(503).send('Site is currently down. Check back soon!')
+// // })
+
+// app.use(express.json())
+// app.use(userRouter)
+// app.use(taskRouter)
+
+// app.listen(port, () => {
+//     console.log('Server is up on port ' + port)
+// })
 
 // const jwt = require('jsonwebtoken')
 
