278951c1d911d723c6681c688cda74f2fe83de7e
codershona <falgunimst95@gmail.com>
2020-07-13 13:34:19 +0200

    Environment Variables

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
+const port = process.env.PORT
+
+
+
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+
+
 
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
@@ -78,13 +93,13 @@ const port = process.env.PORT || 3000
 
 
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+// app.use(express.json())
+// app.use(userRouter)
+// app.use(taskRouter)
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+// app.listen(port, () => {
+//     console.log('Server is up on port ' + port)
+// })
 
 
 
