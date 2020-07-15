2602a8a8a2fb245a27558c2fb15002c5f5fce1a7
codershona <falgunimst95@gmail.com>
2020-07-15 12:35:12 +0200

    Jest testing framework

---
+++ tests/math.test.js
@@ -0,0 +1,29 @@
+test('Hello baby world!', () => {
+
+
+
+    
+
+
+})
+
+
+
+
+
+test('This shoulf fail', () => {
+  
+   throw new Error('Failure!')
+
+
+})
+
+
+
+// - Saves time
+// - Creates reliable software
+// - Gives flexibility to developers
+// - Refactoring
+// - Collaborating
+// - Profiling
+// - finish it
