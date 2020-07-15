fbb71bd6aee2e3ab2915ab30ec61e0c1f0406f81
codershona <falgunimst95@gmail.com>
2020-07-15 12:49:48 +0200

    writitng tests and assertions

---
+++ src/math.js
@@ -0,0 +1,18 @@
+const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
+
+	
+
+	// const tip = total * tipPercent
+
+	// const tip = total * tipPercent + total
+
+	// return total + tip
+
+	// return total + tip + tip
+
+// }
+
+
+module.exports = {
+	calculateTip
+}
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,22 +1,45 @@
-test('Hello baby world!', () => {
+const { calculateTip } = require('../src/math')
 
 
+test('Should calculate total with tip', () => {
 
+	const total = calculateTip(10, .3)
     
+     // if (total !== 13) {
+
+     // 	throw new Error('Total tip should be 13. Got ' + total)
+
+
+     // }
+
+     expect(total).toBe(13)
+
+
+})
+
+
+test('Should calculate total with default tip', () => {
+
+	const total = calculateTip(10)
+
+	expect(total).toBe(12.5)
 
 
 })
 
 
 
+// test('Hello baby world!', () => {
+
+// })
 
 
-test('This shoulf fail', () => {
+// test('This shoulf fail', () => {
   
-   throw new Error('Failure!')
+//    throw new Error('Failure!')
 
 
-})
+// })
 
 
 
