8cea5aa26dacb15491cfc75ee2d347bebd6ebb11
codershona <falgunimst95@gmail.com>
2020-07-16 13:09:52 +0200

    Testiung with Task data

--- tests/fixtures/task.test.js
+++ tests/fixtures/task.test.js
@@ -5,7 +5,16 @@ const app = require('../src/app')
 
 const Task = require('../src/models/task')
 
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { 
+   userOneId, 
+   userOne, 
+   userTwoId,
+   userTwo,
+   taskOne,
+   taskTwo,
+   taskThree,
+   setupDatabase 
+} = require('./fixtures/db')
 
 
 beforeEach(setupDatabase)
@@ -30,3 +39,41 @@ test('Should create task for user', () => {
   
 
 })
+
+
+
+test('Should fetch user tasks', async () => {
+
+   const response = await request(app)
+
+   .get('/tasks')
+   .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+
+   .send()
+
+   .expect(200)
+
+   expect(response.body.length).toEqual(2)
+
+
+})
+
+
+
+test('Should not delete other users tasks', async () => {
+
+   const response = await request(app)
+
+   .delete(`/tasks/${taskOne._id}`)
+   .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+
+   .send()
+   .expect(404)
+
+   const task = await Task.findById(taskOne._id)
+
+   expect(task).not.toBeNull()
+
+
+})
+
